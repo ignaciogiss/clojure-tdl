37988da915d6adfd4c40d8402fbc65a1e0b29cda
Lancel <maurotoscano2@gmail.com>
2019-05-20 00:51:07 -0300

    Made a better example with futures

--- src/clojure_tdl/core.clj
+++ src/clojure_tdl/core.clj
@@ -12,11 +12,19 @@
 
 (defn future-intensive-calculation
   [paramater]
-  (future  (Thread/sleep 5000) (* paramater paramater)))
+  (future  (Thread/sleep 4000) (* paramater paramater)))
   
  
 (defn -main
   [& args]
-  (def parameters (seq(range 10)))
-  (doseq [x parameters]  
-    (println (future-intensive-calculation x))))
+  (def futureA (future-intensive-calculation 2)) 
+  (def futureB (future-intensive-calculation 5))
+  (def futureC (future-intensive-calculation 10))
+  (def futureD (future-intensive-calculation 25))
+  (def futureE (future-intensive-calculation 100))
+  (Thread/sleep 5000)
+  (println @futureA)
+  (println @futureB)
+  (println @futureC)
+  (println @futureD)
+  (println @futureE))
