ff929c4d63ca886fcdac2ffc27b8949edeb19113
Lancel <maurotoscano2@gmail.com>
2019-06-18 18:05:27 -0300

    Movido el ejemplo de futures a una carpeta propia

--- machine-learning/src/clojure_tdl/core.clj
+++ machine-learning/src/clojure_tdl/core.clj
@@ -20,7 +20,12 @@
       modelo2 (fit data 2)
       modelo3 (fit data 3)
       modelo4 (fit data 4)
-      modelo5 (fit data 5)]
+      modelo5 (fit data 5)
+      modelo6 (fit data 6)
+      modelo7 (fit data 7)
+      modelo8 (fit data 8)
+      modelo9 (fit data 9)
+      modelo10 (fit data 10)]
     into-array[modelo1 modelo2 modelo3 modelo4 modelo5]))
 
 (defn crear-modelos-concurrentemente
@@ -30,7 +35,12 @@
       modelo2 (future(fit data 2))
       modelo3 (future(fit data 3))
       modelo4 (future(fit data 4))
-      modelo5 (future(fit data 5))]
+      modelo5 (future(fit data 5))
+      modelo6 (future(fit data 6))
+      modelo7 (future(fit data 7))
+      modelo8 (future(fit data 8))
+      modelo9 (future(fit data 9))
+      modelo10 (future(fit data 10))]
     into-array[@modelo1 @modelo2 @modelo3 @modelo4 @modelo5]))
   
 
@@ -62,22 +72,21 @@
 ;;   (prediccion-modelo-2-flor1, prediccion-modelo2-flor2 ...) ...)
 (defn predecir-con-modelos
   ([lista-modelos flores lista-predicciones]
-   (println (first lista-predicciones))
    (if-not (empty? lista-modelos) 
      (predecir-con-modelos
        (rest lista-modelos)
        flores
        (cons(nearest-neighbors-predict (first lista-modelos) flores)
          lista-predicciones))
-    ;;else
-    lista-predicciones))
+     ;;else
+     lista-predicciones))
   ([lista-modelos flores]
    (predecir-con-modelos lista-modelos flores nil)))
 
 (defn -main
-  [& args]
-  (def iris-vec (obtener-iris-data-como-vector))
+  [& args
+   (def iris-vec (obtener-iris-data-como-vector))]
   (def modelos (crear-modelos-secuencialmente iris-vec))
-  (def flores-para-predecir (map butlast iris-vec))
-  (def predictions (nearest-neighbors-predict (first modelos) flores-para-predecir))
-  (view (seq predictions)))
+  (def flores-para-predecir (take-last 3 (map butlast iris-vec)))
+  (def predictions (predecir-con-modelos modelos flores-para-predecir))
+  (println predictions))
